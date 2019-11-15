f7bcb977f2cafc7869b67daa797384b2f1e02ce1
Christopher Monsanto <chris@monsan.to>
2019-11-15 10:21:41 -0500

    ps-import: tests should use dex interface

--- src/test/ps-import.test.ts
+++ src/test/ps-import.test.ts
@@ -1,63 +1,69 @@
 import psImport from '../ps-import';
 import path from 'path';
-
-// Gens here will be 0-indexed
+import { loader } from '../index';
 
 describe('ps-import', () => {
   const data = psImport(path.join(__dirname, '../../vendor/Pokemon-Showdown/data'));
+  const dex = loader.load(data).construct();
+
+  function getGen(n: number) {
+    return dex.gens.find1(({ num }) => num === n);
+  }
+
   test('rby has the original 151', () => {
-    expect(data.gens[0].species.length).toBe(151);
+    // TODO: add a native length attribute?
+    expect(Array.from(getGen(1).species).length).toBe(151);
   });
 
   test('rby has a non-empty learnset', () => {
     let hasNonEmpty = false;
-    for (const specie of data.gens[1].species) {
+    for (const specie of getGen(1).species) {
       hasNonEmpty = hasNonEmpty || specie.learnset.length > 0;
     }
     expect(hasNonEmpty).toBe(true);
   });
 
   test('gsc has 251 pokemon', () => {
-    expect(data.gens[1].species.length).toBe(251);
+    expect(Array.from(getGen(2).species).length).toBe(251);
   });
 
   test('rs has leftovers', () => {
-    expect(data.gens[2].items.find(x => x.name === 'Leftovers')).toBeDefined();
+    expect(getGen(3).items.find(x => x.name === 'Leftovers')).toBeDefined();
   });
 
   test('gs has berserk gene, but rs does not', () => {
-    expect(data.gens[1].items.find(x => x.name === 'Berserk Gene')).toBeDefined();
-    expect(data.gens[2].items.find(x => x.name === 'Berserk Gene')).toBeUndefined();
+    expect(getGen(2).items.find(x => x.name === 'Berserk Gene')).toBeDefined();
+    expect(getGen(3).items.find(x => x.name === 'Berserk Gene')).toBeUndefined();
   });
 
   test('old gen names', () => {
-    expect(data.gens[5].moves.find(x => x.name === 'High Jump Kick')).toBeDefined();
-    expect(data.gens[5].moves.find(x => x.name === 'Hi Jump Kick')).toBeUndefined();
-    expect(data.gens[4].moves.find(x => x.name === 'High Jump Kick')).toBeUndefined();
-    expect(data.gens[4].moves.find(x => x.name === 'Hi Jump Kick')).toBeDefined();
+    expect(getGen(6).moves.find(x => x.name === 'High Jump Kick')).toBeDefined();
+    expect(getGen(6).moves.find(x => x.name === 'Hi Jump Kick')).toBeUndefined();
+    expect(getGen(5).moves.find(x => x.name === 'High Jump Kick')).toBeUndefined();
+    expect(getGen(5).moves.find(x => x.name === 'Hi Jump Kick')).toBeDefined();
   });
 
   test('z-moves', () => {
-    expect(data.gens[6].moves.find(x => x.name === 'Absorb')?.zMove).not.toBeNull();
-    expect(data.gens[5].moves.find(x => x.name === 'Absorb')?.zMove).toBeNull();
+    expect(getGen(7).moves.find1(x => x.name === 'Absorb').zMove).not.toBeNull();
+    expect(getGen(6).moves.find1(x => x.name === 'Absorb').zMove).toBeNull();
   });
 
   test('cap', () => {
-    expect(data.gens[2].species.find(x => x.name === 'Syclant')).toBeUndefined();
-    expect(data.gens[3].species.find(x => x.name === 'Syclant')?.isNonstandard).toBe('CAP');
-    expect(data.gens[3].species.find(x => x.name === 'Equilibra')).toBeUndefined();
-    expect(data.gens[6].species.find(x => x.name === 'Equilibra')?.isNonstandard).toBe('CAP');
+    expect(getGen(3).species.find(x => x.name === 'Syclant')).toBeUndefined();
+    expect(getGen(4).species.find1(x => x.name === 'Syclant').isNonstandard).toBe('CAP');
+    expect(getGen(4).species.find(x => x.name === 'Equilibra')).toBeUndefined();
+    expect(getGen(7).species.find1(x => x.name === 'Equilibra').isNonstandard).toBe('CAP');
   });
 
   test('altBattleFormes', () => {
-    const venusaurId = data.gens[6].species.findIndex(x => x.name === 'Venusaur');
-    const venusaurMegaId = data.gens[6].species.findIndex(x => x.name === 'Venusaur-Mega');
-    expect(data.gens[6].species[venusaurId].isBattleOnly).toBe(false);
-    expect(data.gens[6].species[venusaurMegaId].isBattleOnly).toBe(true);
-    expect(data.gens[6].species[venusaurId].altBattleFormes).toStrictEqual([venusaurMegaId]);
-    expect(data.gens[6].species[venusaurMegaId].altBattleFormes).toStrictEqual([venusaurId]);
+    const venusaur = getGen(7).species.find1(x => x.name === 'Venusaur');
+    const venusaurMega = getGen(7).species.find1(x => x.name === 'Venusaur-Mega');
+    expect(venusaur.isBattleOnly).toBe(false);
+    expect(venusaurMega.isBattleOnly).toBe(true);
+    expect(venusaur.altBattleFormes).toStrictEqual([venusaurMega]);
+    expect(venusaurMega.altBattleFormes).toStrictEqual([venusaur]);
     // shouldn't include out-of-battle otherFormes
-    expect(data.gens[6].species.find(x => x.name === 'Rotom')?.altBattleFormes).toStrictEqual([]);
+    expect(getGen(7).species.find1(x => x.name === 'Rotom').altBattleFormes).toStrictEqual([]);
   });
 
   test('JSON roundtrippable', () => {
