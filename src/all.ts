ba3840f98a3f738c9a4da7b4a014770d06faca4a
Christopher Monsanto <chris@monsan.to>
2019-11-19 12:37:54 -0500

    all: remove default export
    Unlike Babel, TypeScript default exports do not play nicely with
    CommonJS require().

--- src/all.ts
+++ src/all.ts
@@ -2,6 +2,6 @@ import psImport from './ps-import';
 import path from 'path';
 import { loader } from './index';
 
-export default loader
+export const dex = loader
   .load(psImport(path.join(__dirname, '../vendor/Pokemon-Showdown/data')))
   .construct();
